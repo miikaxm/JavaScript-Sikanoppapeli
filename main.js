2d04f95f1d6b83a6cfa94d50abf3ffdc9bc0ebcf
Miika Valkonen <miika.valkonen08@gmail.com>
2025-05-14 12:34:45 +0300

    Refactor game setup and UI components for Sikanoppapeli

--- main.js
+++ main.js
@@ -1,41 +1,99 @@
-document.getElementById("kuva-1").addEventListener("click", heitaNoppa1)
-// document.getElementById("kuva-2").addEventListener("click", heitaNoppa2)
-document.getElementById("btnAmount").addEventListener("click", Asetamaara)
-document.getElementById("btnNames").addEventListener("click", lisaaPelaaja)
+document.getElementById("btnAloita").addEventListener("click", startGame)
+document.getElementById("dice").addEventListener("click", rollDice)
 
-let pelaajaNimet = []
-let pelaajaMaara = null
+let dices = null
+let players = null
+let playersIds = []
+let listMade = false
+let turn = 0
 
-function Asetamaara(event){
-    event.preventDefault();
-    pelaajaMaara = document.getElementById("playerAmount").value
-    console.log("Pelaaja määrä on nyt: " + pelaajaMaara)
-}
-
-function lisaaPelaaja(event){
-    event.preventDefault();
-    if (pelaajaNimet.length >= pelaajaMaara) {
-        console.log("Et voi lisätä enempää kuin valitsemasi pelaaja määrä")
-        return
+function startGame(){
+    players = parseInt(document.getElementById("pelaajienMäärä").value)
+    dices = parseInt(document.getElementById("noppaMäärä").value)
+    if (dices == 1){
+        document.getElementById("startScreen").hidden = true;
+        document.getElementById("oneDiceArea").hidden = false;
+        document.getElementById("pointsArea").hidden = false;
+    } else {
+        document.getElementById("startScreen").hidden = true;
+        document.getElementById("twoDiceArea").hidden = false;
+        document.getElementById("pointsArea").hidden = false;
     }
-    let nimi = document.getElementById("PlayerNames").value
-    pelaajaNimet.push(nimi)
-    console.log("Lisättiin pelaajiin:" + " " + nimi)
-
-    const uusiPelaaja = document.createTextNode(nimi + ": 0 ");
-    const br = document.createElement("br");
-    document.getElementById("pisteet").appendChild(uusiPelaaja);
-    document.getElementById("pisteet").appendChild(br);
+    addPlayers()
+    updatePointsList()
 }
 
-function heitaNoppa1(){
-    firstRandomNumber = Math.floor(Math.random()* 6 + 1)
-    firstImage = "img/dice_" + firstRandomNumber + ".png"
-    document.getElementById("kuva-1").setAttribute("src", firstImage)
+function getRandomNumber(){
+    return Math.floor(Math.random()* 6 + 1)
 }
 
-function heitaNoppa2(){
-    secondRandomNumber = Math.floor(Math.random()* 6 + 1)
-    secondImage = "img/dice_" + secondRandomNumber + ".png"
-    document.getElementById("kuva-2").setAttribute("src", secondImage)
+function updatePointsList(){
+    if (listMade == false){
+        playersIds.forEach(player => {
+        const test = document.createElement("h2")
+        const newPlayer = document.createTextNode(player.name + ": 0 ");
+        test.appendChild(newPlayer)
+        test.setAttribute("id", player.name)
+        document.getElementById("pointsArea").appendChild(test);
+        listMade = true
+    });
+    } else {
+        let kohta = document.getElementById("Pelaaja " + (turn + 1))
+        kohta.innerText = playersIds[turn].name + ": " + playersIds[turn].savedPoints
+    }
+}
+
+function addPlayers(){
+    let i = 1
+    while (i < players+1) {
+        let object = {name: "Pelaaja "+i, points: 0, savedPoints: 0}
+        playersIds.push(object)
+        i += 1
+    }
+}
+
+function rollDice(){
+    const rolledNumber = getRandomNumber()
+    rolledImage = "img/dice_" + rolledNumber + ".png"
+    document.getElementById("dice").setAttribute("src", rolledImage)
+    console.log(playersIds[turn])
+    if (rolledNumber != 1){
+        playersIds[turn].points += rolledNumber
+        document.getElementById("currentScore").innerText = "Tämän hetkiset pisteet: " +  playersIds[turn].points
+        document.getElementById("btnStopRolling").addEventListener("click", savePoints)
+    } else {
+        playersIds[turn].points = 0
+        document.getElementById("currentScore").innerText = "Tämän hetkiset pisteet: " +  playersIds[turn].points
+        nextTurn()
+    }
+}
+
+function savePoints(){
+    playersIds[turn].savedPoints += playersIds[turn].points
+    playersIds[turn].points = 0
+    document.getElementById("currentScore").innerHTML = "Tämän hetkiset pisteet: " +  "0"
+    updatePointsList()
+    checkWin()
+    nextTurn()
+}
+
+function nextTurn(){
+    if (turn == players-1){
+        turn = 0
+    } else {
+        turn += 1
+    }
+    document.getElementById("currentTurn").innerText = "Pelaajan vuoro: " + playersIds[turn].name
+    updatePointsList()
+}
+
+function checkWin(){
+    playersIds.forEach(players => {
+        if (players.savedPoints >= 100){
+            document.getElementById("oneDiceArea").hidden = true;
+            document.getElementById("pointsArea").hidden = true;
+            document.getElementById("winScreen").hidden = false;
+            document.getElementById("voittajaTeksti").innerText = "Pelin on voittanut " +players.name;
+        }
+    });
 }
